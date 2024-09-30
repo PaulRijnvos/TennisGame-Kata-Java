8a06ce47a5424cef7448c1795c1d15c29c558769
Paul Rijnvos <PaulRijnvos@GMail.com>
2024-09-30 12:28:09 +0200

    Code reformatting

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,68 +1,71 @@
 public class TennisGame1 implements TennisGame
 {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-
-    public void wonPoint(String playerName)
+    public class TennisGame1 implements TennisGame
     {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
-    }
 
-    public String getScore()
-    {
-        String score = "";
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+        private int player1Score = 0;
+        private int player2Score = 0;
 
-            }
+        public void wonPoint(String playerName)
+        {
+            if (playerName == "player1")
+                player1Score += 1;
+            else
+                player2Score += 1;
         }
-        else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-        }
-        else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
+
+        public String getScore()
+        {
+            String score = "";
+            int tempScore = 0;
+            if (player1Score == player2Score) {
+                switch (player1Score) {
                     case 0:
-                        score += "Love";
+                        score = "Love-All";
                         break;
                     case 1:
-                        score += "Fifteen";
+                        score = "Fifteen-All";
                         break;
                     case 2:
-                        score += "Thirty";
+                        score = "Thirty-All";
                         break;
-                    case 3:
-                        score += "Forty";
+                    default:
+                        score = "Deuce";
                         break;
+
                 }
             }
+            else if (player1Score >= 4 || player2Score >= 4) {
+                int minusResult = player1Score - player2Score;
+                if (minusResult == 1) score = "Advantage player1";
+                else if (minusResult == -1) score = "Advantage player2";
+                else if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+            }
+            else {
+                for (int i = 1; i < 3; i++) {
+                    if (i == 1) tempScore = player1Score;
+                    else {
+                        score += "-";
+                        tempScore = player2Score;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            score += "Love";
+                            break;
+                        case 1:
+                            score += "Fifteen";
+                            break;
+                        case 2:
+                            score += "Thirty";
+                            break;
+                        case 3:
+                            score += "Forty";
+                            break;
+                    }
+                }
+            }
+            return score;
         }
-        return score;
     }
-}
